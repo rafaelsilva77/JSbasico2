9e05fdd655748f1195e341a3cb9858dac59dcf65
Rafael dos Santos Silva <githubrafael9@gmail.com>
2026-01-03 03:47:36 -0300

    criei exercício da aula 5

---
+++ aula05/index.js
@@ -0,0 +1,52 @@
+
+/* // operadores de atribuição e aritméticos
+var valor1, valor2, total;
+valor1 = 10;
+valor2 = 20;
+ valor1 += valor2; // operador de atribuição 
+valor1 -= valor2; // operador de atribuição 
+valor1 = valor1 + valor2; // mesma coisa que a linha acima 
+total = valor1 + valor2; // operador aritmético 
+*/
+
+
+/* // operador de concatenação
+var valor1, valor2, total;
+valor1 = "João ";
+valor2 = "da Silva";
+total = valor1 + valor2; // operador de concatenação ouoperador de sequência
+alert(total);
+ */
+
+
+/* // operadores de comparação
+var valor1, valor2, total;
+valor1 = "8";
+valor2 = 8;
+total = (valor1 === valor2); // operador de comparação estrita
+total = (valor1 == valor2); // operador de comparação
+total = (valor1 != valor2); // operador de diferença
+total = (valor1 !== valor2); // operador de diferença estrita   
+total = (valor1 > valor2); // operador maior que
+total = (valor1 < valor2); // operador menor que
+total = (valor1 >= valor2); // operador maior ou igual a
+total = (valor1 <= valor2); // operador menor ou igual a
+alert(total); 
+*/
+
+/* // operador ternário
+var idade, eleitor;
+idade = 18;
+eleitor = (idade < 18) ? "Não é eleitor" : "É eleitor"; // operador ternário
+alert(`A resposta é: ${eleitor} a idade dele é ${idade} anos.`); 
+*/
+
+/* // operadores lógicos
+var idade, eleitor,resultado;
+idade = 65;
+eleitor = (idade < 18) ? "Não é eleitor" : "É eleitor"; // operador ternário
+
+resultado = (idade > 60 && idade < 70); // operador lógico true, false os dois tem que ser verdadeiros
+resultado = (idade > 60 || idade < 70); // operador lógico true, falseou um dos dois tem que ser verdadeiro
+resultado = !(idade > 60); // operador lógico true, false 
+// */
